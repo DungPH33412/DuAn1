0acf734c707f7fe4dc2ae552ff4b6b3c9686d876
dhuy1704 <duchuy170404@gmail.com>
2023-11-29 18:10:21 +0700

    huy

--- app/src/main/java/com/example/duan1/Model/Detailnvoice.java
+++ app/src/main/java/com/example/duan1/Model/Detailnvoice.java
@@ -1,4 +1,50 @@
 package com.example.duan1.Model;
 
 public class Detailnvoice {
+    private String idKey;
+    private String idProduct;
+    private String idInvoice;
+    private String quantityProduct;
+
+    public Detailnvoice() {
+    }
+
+    public Detailnvoice(String idKey, String idProduct, String idInvoice, String quantityProduct) {
+        this.idKey = idKey;
+        this.idProduct = idProduct;
+        this.idInvoice = idInvoice;
+        this.quantityProduct = quantityProduct;
+    }
+
+    public String getIdKey() {
+        return idKey;
+    }
+
+    public void setIdKey(String idKey) {
+        this.idKey = idKey;
+    }
+
+    public String getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(String idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getIdInvoice() {
+        return idInvoice;
+    }
+
+    public void setIdInvoice(String idInvoice) {
+        this.idInvoice = idInvoice;
+    }
+
+    public String getQuantityProduct() {
+        return quantityProduct;
+    }
+
+    public void setQuantityProduct(String quantityProduct) {
+        this.quantityProduct = quantityProduct;
+    }
 }
